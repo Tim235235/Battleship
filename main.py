ea0647eae74d2f82305a5d8433895ebf7f5ea5a1
Tim235235 <timofeysona@gmail.com>
2023-02-22 18:53:14 +0100

    Started using dictionaries to create a board

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
 import numpy as np
-'''
-game_board = np.array([
-        ["", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
-        ["1", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
-        ["2", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
-        ["3", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
-        ["4", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
-        ["5", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
-        ["6", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
-        ["7", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
-        ["8", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
-        ["9", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"],
-        ["10", "-", "-", "-", "-", "-", "-", "-", "-", "-", "-"]
-        ]
-    )
-'''
+
+# Functions
 
 
-# Ship placements & printing/clearing the board
+def ship_placement(player):
+    # 0 -> 10
+    for i in range(11):
+        for b in range(11):
+            player["cords"].update([((i, b), "-")])
+    moves = 0
+    counter = 0
+    while moves != 20:
+        x = int(input(": "))
+        y = int(input(": "))
+        if x in allowed_moves() and y in allowed_moves():
+            player["cords"].update([((y, x), "X")])
+            # Создать функцию для печати поля
+            # Вернуть буквы и цифры
+            for i in range(11):
+                print(i + 1, end=" ")
+                for b in range(11):
+                    print(player["cords"][(i, b)], end=" ")
+                    counter += 1
+                    if counter % 11 == 0:
+                        print("\n")
+            moves += 1
+        else:
+            print("Try again")
+    for i in range(100):
+        print()
 
-# Поправить вызов каждый раз
+# Вызывать функцию только один раз, в начале игры!
 def allowed_moves():
     possible_moves = []
     for i in range(1, 11):
@@ -29,106 +40,60 @@ def allowed_moves():
     return available_moves
 
 
-def player_ship_placement(player):
-    while len(player) != 3:
-        print("Enter coords")
-        x = int(input(": "))
-        y = int(input(": "))
-        if x in allowed_moves() and y in allowed_moves() and player.count([y, x]) == 0:
-            player.append([y, x])
-            game_board[x][y] = "X"
-            board()
-        else:
-            print("Try again those coordinates don't work")
-    for i in range(100):
-        print()
-    clearing_board()
-
-
-def clearing_board():
-    while np.count_nonzero(game_board == "X") != 0 or np.count_nonzero(game_board == "H") != 0:
-        for row_index, row in enumerate(game_board):
-            for col_index, cell in enumerate(row):
-                if cell == "X" or cell == "H":
-                    game_board[row_index][col_index] = "-"
-    return game_board
-
-
-def board():
-    for i in range(len(game_board)):
-        print(*game_board[i])
-
-
-def checking_for_hit(player_cords):
-    for i in range(len(player_cords)):
-        if len(player_cords[i]) == 2:
-            game_board[player_cords[i][1]][player_cords[i][0]] = "X"
-        elif len(player_cords[i]) == 3:
-            game_board[player_cords[i][1]][player_cords[i][0]] = "H"
-    board()
-
-
-def player_turn(player, enemy):
-    print("|Ship checker|\n")
-    clearing_board()
+def player_turn(enemy, player):
     while True:
         x = int(input(": "))
         y = int(input(": "))
-        if x in allowed_moves() and y in allowed_moves() and player.count([y, x]) == 0:
-            if [y, x] in enemy:
-                enemy.remove([y, x])
-                player.append([y, x, "H"])
-                print("You hit an enemy ship")
+        if x in allowed_moves() and y in allowed_moves():
+            if enemy["cords"][(y, x)] == "X":
+                player["Hits"].append((y, x))
+                print("You hit an enemy ship! Congratulations")
+                print(player)
                 break
             else:
-                player.append([y, x])
+                player["Misses"].append((y, x))
+                print("You missed the ship :(")
+                print("Don't worry you'll get it next time!")
+                print(player)
                 break
         else:
             print("try again")
 
 
+# Player dictionaries
+player_1 = {"cords": {}}
+player_2 = {"cords": {}}
+board_checker_1 = {"cords": {}}
+board_checker_2 = {"cords": {}}
+player_1_shots = {"Misses": [], "Hits": []}
+player_2_shots = {"Misses": [], "Hits": []}
 
-# pl1 -> pl2 : 0, 0 X
-# 1
-# 22
-# 333
-# 4444
-'''
-counter = 0
-for key in game_board:
-    if counter % 10 == 0:
-        print("\n")
-    print(game_board[key])
-    counter += 1
+# Main action
+print("Start by placing your ships on the game board. You will need to cover 20 tiles in total. "
+      "Here are the ship requirements:\n")
+print("--> x4 one tile-ships")
+print("--> x3 2 tile-ships")
+print("--> x2 three tile-ships")
+print("--> x1 4 tile-ships\n")
+print("-----------------------------------------------------------------------------------------------------------")
+print("Player 1 turn")
+ship_placement(player_1)
+print("Player 2 turn")
+ship_placement(player_2)
 
-'''
-game_board = {[0, 0]: '2', [0, 1]: '2'}
-
-player_one_cords = []
-player_two_cords = []
-
-player_one_moves = []
-player_two_moves = []
-
-print("|Player 1 turn|:\n")
-player_ship_placement(player_one_cords)
-print("|Player 2 turn|:\n")
-player_ship_placement(player_two_cords)
-
-flag = True
-while flag:
+while True:
+    # Создать универсальную функцию
     print("Player 1 turn")
-    player_turn(player_one_moves, player_two_cords)
-    print(player_one_moves)
-    checking_for_hit(player_one_moves)
-    if len(player_two_cords) == 0:
+    player_turn(player_2, player_1_shots)
+    print("-----------------------------------------------------------------------------------------------------------")
+    if len(player_1_shots["Hits"]) == 20:
+        print("Player 1 won")
         break
-    print("---------------------------------------------------------------------------------"
-          "-----------------------------------------------")
+
+
     print("Player 2 turn")
-    player_turn(player_two_moves, player_one_cords)
-    checking_for_hit(player_two_moves)
-    if len(player_one_cords) == 0:
+    player_turn(player_1, player_2_shots)
+    print("-----------------------------------------------------------------------------------------------------------")
+    if len(player_2_shots["Hits"]) == 20:
+        print("Player 2 won")
         break
-    print("---------------------------------------------------------------------------------"
-          "-----------------------------------------------")
